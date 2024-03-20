beecb8329807b4d053423594ca3001e3f7d63040
SwathiKSuresh <swathiksuresh25@gmail.com>
2024-03-20 11:15:28 +0530

    registration form created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
 function App() {
   return (
     <div>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Registration/>} />
+          <Route path='/login' element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
